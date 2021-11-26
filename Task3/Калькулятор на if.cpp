39ba4af6f4768107a707a69e4abc6095433494b2
DoctorStrang314 <dmitry495d354@gmail.com>
2021-11-26 19:39:06 +0300

    add Project

---
+++ Task3/Калькулятор на if.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(0, "");
+    int num1, num2;
+    char oper;
+    cout << "Введите первое число ";
+    cin >> num1;
+    cout << "Введите второе число ";
+    cin >> num2;
+    cout << "Введите действие ";
+    cin >> oper;
+   
+   
+    cout << num1 << oper << num2<<"=";
+    if (oper == '+') {
+        cout << num1 + num2;
+    }
+    else if (oper == '-') {
+        cout << num1 - num2;
+    }
+    else if (oper == '*') {
+        cout << num1 * num2;
+    }
+    else if (oper == '/' && num2 != 0) {
+        cout << num1 / num2;
+    }
+    else if (oper == '%') {
+        cout << num1 % num2;
+    }
+    else {
+        cout << "\nЧто-то пошло не так";
+    }
+    return 0;
+}
+
